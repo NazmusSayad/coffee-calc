db0a42a78d3e6cddaa67f2b6d42272defe8748ad
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-11-28 15:53:02 +0600

    ?? .gitignore ?? .npmignore ?? package-lock.json ?? package.json ?? src/ ?? tsconfig.json ?? yarn.lock

---
+++ src/index.ts
@@ -0,0 +1,116 @@
+import { r } from 'rype'
+
+export const coffeeCalculatorOptions = r.object({
+  coffeeRatio: r.number().min(0).default(1),
+  sugarRatio: r.number().min(0).default(1),
+  creamerRatio: r.number().min(0).default(1),
+  waterRatio: r.number().min(0).default(24),
+  superAccurate: r.boolean().default(false),
+})
+
+export default class CoffeeCalculator {
+  private coffee: number
+  private sugar: number
+  private creamer: number
+  private water: number
+  private ingredients: number
+  private total: number
+
+  constructor(ratio: r.inferInput<typeof coffeeCalculatorOptions>) {
+    const conf = coffeeCalculatorOptions.parse(ratio)
+
+    const coffee = conf.coffeeRatio
+    const sugar = conf.sugarRatio
+    const creamer = conf.creamerRatio
+    const water = conf.waterRatio
+    const ingredients = coffee + sugar + creamer
+
+    const ingredientsLoss = conf.superAccurate ? ingredients * 0.01 : 0
+    const total = ingredients - ingredientsLoss + water
+
+    this.coffee = coffee
+    this.sugar = sugar
+    this.creamer = creamer
+    this.water = water
+    this.ingredients = ingredients
+    this.total = total
+  }
+
+  getByCoffee(amount: number) {
+    const coffee = amount
+    const ratio = amount / this.coffee
+    return {
+      coffee,
+      sugar: this.sugar * ratio,
+      creamer: this.creamer * ratio,
+      ingredients: this.ingredients * ratio,
+      water: this.water * ratio,
+      total: this.total * ratio,
+    }
+  }
+
+  getBySugar(amount: number) {
+    const sugar = amount
+    const ratio = amount / this.sugar
+    return {
+      coffee: this.coffee * ratio,
+      sugar,
+      creamer: this.creamer * ratio,
+      ingredients: this.ingredients * ratio,
+      water: this.water * ratio,
+      total: this.total * ratio,
+    }
+  }
+
+  getByCreamer(amount: number) {
+    const creamer = amount
+    const ratio = amount / this.creamer
+    return {
+      coffee: this.coffee * ratio,
+      sugar: this.sugar * ratio,
+      creamer,
+      ingredients: this.ingredients * ratio,
+      water: this.water * ratio,
+      total: this.total * ratio,
+    }
+  }
+
+  getByWater(amount: number) {
+    const water = amount
+    const ratio = amount / this.water
+    return {
+      coffee: this.coffee * ratio,
+      sugar: this.sugar * ratio,
+      creamer: this.creamer * ratio,
+      ingredients: this.ingredients * ratio,
+      water,
+      total: this.total * ratio,
+    }
+  }
+
+  getByIngredients(amount: number) {
+    const ingredients = amount
+    const ratio = amount / this.ingredients
+    return {
+      coffee: this.coffee * ratio,
+      sugar: this.sugar * ratio,
+      creamer: this.creamer * ratio,
+      ingredients,
+      water: this.water * ratio,
+      total: this.total * ratio,
+    }
+  }
+
+  getByTotal(amount: number) {
+    const total = amount
+    const ratio = amount / this.total
+    return {
+      coffee: this.coffee * ratio,
+      sugar: this.sugar * ratio,
+      creamer: this.creamer * ratio,
+      ingredients: this.ingredients * ratio,
+      water: this.water * ratio,
+      total,
+    }
+  }
+}
